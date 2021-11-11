cc4a4ea0499e78029985fa2ced031c31d102c062
frederic8 <81454733+frederic8@users.noreply.github.com>
2021-11-11 14:47:40 +0100

    Update variables let to const

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 // créer une function qui créer un element "section" qui contient la classList "result"
 const functionSection = () => {
-  let section = document.createElement("section");
+  const section = document.createElement("section");
   section.classList.add("result");
   return section;
 };
 
 // créer une function qui créer un element "div" qui contient la classList "result"
 const functionDiv = (result) => {
-  let div = document.createElement("div");
+  const div = document.createElement("div");
   div.classList.add(result);
   return div;
 };
 
 // créer une function qui créer un element "i" qui contient les classList "fas" & "fa-bookmark"
 const functionSave = () => {
-  let i = document.createElement("i");
+  const i = document.createElement("i");
   i.classList.add("fas");
   i.classList.add("fa-bookmark");
   return i;
@@ -22,7 +22,7 @@ const functionSave = () => {
 
 // créer une function qui créer un element i qui contient les classList "fas" & "fa-trash"
 const functionTrash = () => {
-  let i = document.createElement("i");
+  const i = document.createElement("i");
   i.classList.add("fas");
   i.classList.add("fa-trash");
   return i;
@@ -30,55 +30,55 @@ const functionTrash = () => {
 
 // créer une function qui créer un element h3
 const functionH3 = () => {
-  let h3 = document.createElement("h3");
+  const h3 = document.createElement("h3");
   return h3;
 };
 
 // créer une function qui créer un element p
 const functionP = () => {
-  let p = document.createElement("p");
+  const p = document.createElement("p");
   return p;
 };
 
 // créer une function qui créer un element img
 const functionImg = () => {
-  let img = document.createElement("img");
+  const img = document.createElement("img");
   return img;
 };
 
 // créer une function qui créer un element form
 const functionForm = () => {
-  let form = document.createElement("form");
+  const form = document.createElement("form");
   return form;
 };
 
 // créer une function qui créer un element label
 const functionLabel = () => {
-  let label = document.createElement("label");
+  const label = document.createElement("label");
   return label;
 };
 
 // créer une function qui créer un element input
 const functionInput = () => {
-  let input = document.createElement("input");
+  const input = document.createElement("input");
   return input;
 };
 
 // créer une function qui créer un element button
 const functionButton = () => {
-  let button = document.createElement("button");
+  const button = document.createElement("button");
   return button;
 };
 
 // créer une function qui créer un element hr
 const functionHr = () => {
-  let hr = document.createElement("hr");
+  const hr = document.createElement("hr");
   return hr;
 };
 
 // créer une function qui créer un element h2
 const functionH2 = () => {
-  let h2 = document.createElement("h2");
+  const h2 = document.createElement("h2");
   return h2;
 };
 
